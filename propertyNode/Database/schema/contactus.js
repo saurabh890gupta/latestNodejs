7a1b413dea9a83c68ee79aa0090ca7e87190d85c
saurabh890gupta <saurabh.gup890@gmail.com>
2019-09-11 11:39:21 +0530

    first pro

---
+++ propertyNode/Database/schema/contactus.js
@@ -0,0 +1,14 @@
+const mongoose=require('mongoose');
+let Schema=mongoose.Schema;
+
+var  Contactus= new Schema({
+    id: String,
+    name:String,
+    email:String,
+    contact:String,
+    address:String,
+    query:String,
+    user_id:String,
+},{collection:'Contactus'})
+
+module.exports=mongoose.model('Contactus', Contactus);
